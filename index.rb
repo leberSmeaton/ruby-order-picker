92f1cb3510542ef2a07c41886a4c67f2f4abc171
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-05-04 12:10:27 +1000

    implements random group

--- index.rb
+++ index.rb
@@ -1,12 +1,12 @@
 # create empty array
-
-group = []
-
 # build menu system
 # choices 1, 2, 3.
 # case statement + while loop
 
+group = ["Alice", "Bob", "Charlie", "Dylan"]
+
 while true
+    puts "There are #{group.length} memebers in the group"
     puts "Press 1 to add a member to the group"
     puts "Press 2 to display a random group"
     puts "Press 3 to quit"
@@ -20,7 +20,21 @@ while true
         group.push(name)
     when 2
         puts "Random group output: "
-        # To do - output random group
+        random_order_loop_running = true
+        while random_order_loop_running
+            copied_group = group.clone
+            while copied_group.length > 0 
+                random_index = rand(copied_group.length -1)
+                puts copied_group[random_index].capitalize
+                copied_group.delete_at(random_index)
+            end
+            puts "Press 1 to repeat"
+            puts "Press 2 to quit"
+            choice = gets.chomp.to_i
+            if choice == 2
+                exit
+            end
+        end
     when 3
         
         # To do - check they want to quit
@@ -28,7 +42,7 @@ while true
     else 
         puts "Invalid input please try again"
     end
-    sleep(3)
+    sleep(2)
     system "clear"
 end
 
